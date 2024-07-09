9507616da6b052522e5ed3e89ee1da2d603c6eca
Christopher Grande <chrisgrande@mac.com>
2024-07-09 14:02:50 -0400

    FMT the unholy spaces

--- munki_script_checks_grande.tf
+++ munki_script_checks_grande.tf
@@ -1,13 +1,13 @@
-resource zentral_munki_script_check "mcs-auditing-audit_acls_files_configure" {
-  name            = "[mSCP - Grande] - Auditing - Configure Audit Log Files to Not Contain Access Control Lists"
-  description     = trimspace(<<EODESC
+resource "zentral_munki_script_check" "mcs-auditing-audit_acls_files_configure" {
+  name = "[mSCP - Grande] - Auditing - Configure Audit Log Files to Not Contain Access Control Lists"
+  description = trimspace(<<EODESC
 The audit log files _MUST_ not contain access control lists (ACLs).
 
 This rule ensures that audit information and audit files are configured to be readable and writable only by system administrators, thereby preventing unauthorized access, modification, and deletion of files.
 EODESC
   )
-  type            = "ZSH_INT"
-  source          = trimspace(<<EOSRC
+  type = "ZSH_INT"
+  source = trimspace(<<EOSRC
 /bin/ls -le $(/usr/bin/grep '^dir' /etc/security/audit_control | /usr/bin/awk -F: '{print $2}') | /usr/bin/awk '{print $1}' | /usr/bin/grep -c ":"
 EOSRC
   )
